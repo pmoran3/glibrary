342cd61c61dfa98dbe828cef4bc40b6f11158410
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2020-03-02 16:24:04 -0500

    reading json into struct

--- goptions/jsonExample.cc
+++ goptions/jsonExample.cc
@@ -9,14 +9,57 @@ using namespace gstring;
 #include <iostream>
 using namespace std;
 
+
+
+// a simple struct to model the detector option
+namespace detector {
+	struct sdet {
+		string detector;
+		string factory;
+		string variation;
+	};
+
+	// JSON I/O functions for easy conversion
+	void to_json(json& j, const sdet& det) {
+		j = json{{"detector", det.detector}, {"factory", det.factory}, {"variation", det.variation}};
+	}
+
+	void from_json(const json& j, sdet& det) {
+		j.at("detector").get_to(det.detector);
+		j.at("factory").get_to(det.factory);
+		j.at("variation").get_to(det.variation);
+	}
+}
+
+
+
 // json parser
 #include "json.hpp"
 using namespace nlohmann;
 
+
+// example of parsing one jcard and reading a variable
 int main(int argc, char* argv[])
 {
+	string fileName = argv[1];
 
-	// add
+	cout << " Opening jcard: " << fileName << endl;
+
+	// removing '#'
+	string parsedJson = parseFileAndRemoveComments(fileName);
+
+	// building parsed json object
+	json pjson = json::parse(parsedJson);
+
+	// careful: this returns an array
+	json jdetectors = pjson["add-detector"];
+
+
+	for (auto det: jdetectors) {
+		cout << " json: " << det << endl;
+		auto d = det.get<detector::sdet>();
+		cout << " detector struct variable: " << d.detector << endl;
+	}
 
 	return 1;
 }
