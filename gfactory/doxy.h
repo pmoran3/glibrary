0f74e7a3ac726e5806dd1632d1bc17e686235f7e
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2020-03-12 16:50:37 -0400

    doxy styles

--- gfactory/doxy.h
+++ gfactory/doxy.h
@@ -1,77 +1,45 @@
-#ifndef  GDL_H
-#define  GDL_H 1
-
-// this code is based on this tutorial:
-// https://0x00sec.org/t/c-dynamic-loading-of-shared-objects-at-runtime/1498
-
-// c++ plugin loading functions
-#include <dlfcn.h>
-
-// c++
-#include <sys/stat.h> // to check if file exists
-#include <string>
-#include <iostream>
-using namespace std;
-
-typedef void* dlhandle;
-
-static dlhandle load_lib(const string& path);
-static void close_lib(dlhandle handle);
-
+#ifndef  GFACTORY_DOXY_H
+#define  GFACTORY_DOXY_H  1
 /**
- * @struct DynamicLib
- * @brief  Structure to load dynamically symbols from a shared library
+ * \mainpage
+ * \section overview Overview
+ * This frameworks provides static or dynamic loading of client factories.\n
+ * The class GManager registers the client factories and provide methods
+ * to instantiate them.
+ * \section howto Suggested Use
+ * After the gManager registration clients can instantiate
+ * the derived class and store them in a map<string, Base>
+ * for later use.
+ * \section staticUse Registering and Instantiating static client factories
+ * In this case the client have access to both the base and the derived class headers.
+ * To register a "Triangle" class derived from "Shape":
+ * <pre>
+ * \#include "ShapeFactory.h"
+ * \#include "Triangle.h"
+ * 
+ *  // map that will contain the derived classes instances
+ * map<string, Shape*> shapes;
+ * GManager manager;
+ *  // register class in the manager
+ * manager.RegisterObjectFactory<Triangle>("triangle");
+ * map["triangle"] = manager.CreateObject<Shape>("triangle");  // retrieve "triangle", instantiate and store new client Triangle class
+ * map["triangle"]->Method();  // Method() is pure virtual in Shape. Calling the instance Triangle method here
+ * </pre>
+ * \section dynamicUse Registering and Instantiating dynamic client factories
+ * In this case the client have access to only the base class header.
+ * The derived classes are compiled in shared libraries.
+ * To register a "ford" class derived from "Car":
+ * <pre>
+ * \#include "Car.h"
+ * map<string, Car*> cars;   // map that will contain the derived classes instances
+ * GManager manager;
+ * manager.registerDL("fordFactory");  // register class in the manager
+ * map["ford"] = manager.LoadObjectFromLibrary<Car>("fordFactory");  // retrieve "fordFactory", instantiate and store new client fordFactory class
+ * map["ford"]->Method();  // Method() is pure virtual in Car. Calling the instance fordFactory method here
+ * </pre>
+ * \n\n
+ * \author \n &copy; Maurizio Ungaro
+ * \author e-mail: ungaro@jlab.org\n\n\n
  */
-// PRAGMA TODO: add verbosity option?
-// add error if DL is not found, with msg
-// add mutex so we can use verbosity cout? see http://en.cppreference.com/w/cpp/memory/shared_ptr
-struct DynamicLib {
-
-	// default constructor
-	DynamicLib() = default;
-
-	string   path;    ///< filename base of the dynamic library
-	dlhandle handle;  ///< posix handle of the dynamic library
-
-
-	/**
-	 * @param p name of the dynamic library
-	 */
-	DynamicLib(string p) : path(p), handle(nullptr) {
-		//	cout << " Loading DL " << p << endl;
-		if(FileExists(path)) {
-			handle = load_lib(p);
-		} else {
-			// PRAGMA TODO: no warning here. But give error if digitization is requested but not found
-			// either exception or mutex lock
-			cout << " ⚠︎ Warning: " << path << " not found." << endl;
-		}
-	}
-
-	~DynamicLib() {
-		if (handle != nullptr)
-			close_lib(handle);
-		//		cout << " Closing DL " << path << endl;
-	}
-
-	bool FileExists(const std::string& name) {
-		struct stat buffer;
-		return (stat (name.c_str(), &buffer) == 0);
-	}
-
-};
-
-
-static dlhandle load_lib(const string& path) {
-
-	return dlopen(path.data() , RTLD_NOW);
-	// get a handle to the lib, may be nullptr.
-	// RTLD_NOW ensures that all the symbols are resolved immediately. This means that
-	// if a symbol cannot be found, the program will crash now instead of later.
-}
-
-static void close_lib(dlhandle handle) {
-	dlclose(handle);
-}
 
 #endif
--- gfactory/gfactory.h
+++ gfactory/gfactory.h
@@ -4,44 +4,6 @@
 // this code is based on TheComet tutorial here:
 // https://www.gamedev.net/topic/683154-c-extend-class-in-dll/
 
-/**
- * \mainpage
- * \section overview Overview
- * This frameworks provides static or dynamic loading of client factories.\n
- * The class GManager registers the client factories and provide methods
- * to instantiate them.
- * \section howto Suggested Use
- * After the gManager registration clients can instantiate
- * the derived class and store them in a map<string, Base>
- * for later use.
- * \section staticUse Registering and Instantiating static client factories
- * In this case the client have access to both the base and the derived class headers.
- * To register a "Triangle" class derived from "Shape":
- * <pre>
- * \#include "ShapeFactory.h"
- * \#include "Triangle.h"
- * map<string, Shape*> shapes;   // map that will contain the derived classes instances
- * GManager manager;
- * manager.RegisterObjectFactory<Triangle>("triangle");  // register class in the manager
- * map["triangle"] = manager.CreateObject<Shape>("triangle");  // retrieve "triangle", instantiate and store new client Triangle class
- * map["triangle"]->Method();  // Method() is pure virtual in Shape. Calling the instance Triangle method here
- * </pre>
- * \section dynamicUse Registering and Instantiating dynamic client factories
- * In this case the client have access to only the base class header.
- * The derived classes are compiled in shared libraries.
- * To register a "ford" class derived from "Car":
- * <pre>
- * \#include "Car.h"
- * map<string, Car*> cars;   // map that will contain the derived classes instances
- * GManager manager;
- * manager.registerDL("fordFactory");  // register class in the manager
- * map["ford"] = manager.LoadObjectFromLibrary<Car>("fordFactory");  // retrieve "fordFactory", instantiate and store new client fordFactory class
- * map["ford"]->Method();  // Method() is pure virtual in Car. Calling the instance fordFactory method here
- * </pre>
- * \n\n
- * \author \n &copy; Maurizio Ungaro
- * \author e-mail: ungaro@jlab.org\n\n\n
- */
 
 // c++
 #include <map>
