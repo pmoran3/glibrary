2acca690a76e19bdd0828b9e76e8d9c59296cd84
maureeungaro <maureeungaro@gmail.com>
2021-05-05 15:46:27 -0400

    added gsetup project

---
+++ gsetup/gvolume.cc
@@ -0,0 +1,94 @@
+// gVolume
+#include "gvolume.h"
+
+// c++
+#include <iostream>
+
+// glibrary
+#include "gstring.h"
+
+using std::cerr;
+using std::endl;
+using gstring::trimSpacesFromString;
+
+GVolume::GVolume(vector<string> pars, string importPath)
+{
+	if( pars.size() != gVolumeNumberOfParameters) {
+		cerr << FATALERRORL << " Fatal Error: Incorrect number of system parameters (" << pars.size() << ") for " << pars[0] ;
+		cerr << " It should be " << gVolumeNumberOfParameters << endl;
+		exit(0);
+	} else {
+		// size is already checked in addVolume, the only interface to volume
+		int i=0;
+		
+		name         = trimSpacesFromString(pars[i++]);
+		mother       = trimSpacesFromString(pars[i++]);
+		description  = trimSpacesFromString(pars[i++]);
+
+		type        = trimSpacesFromString(pars[i++]);
+		parameters  = trimSpacesFromString(pars[i++]);
+		
+		string pvis = trimSpacesFromString(pars[i++]);
+		visible     = (pvis == "1") ? true : false;
+		style       = stoi(trimSpacesFromString(pars[i++]));
+		color       = trimSpacesFromString(pars[i++]);
+		
+		material    = trimSpacesFromString(pars[i++]);
+		emfield     = trimSpacesFromString(pars[i++]);
+		
+		pos         = trimSpacesFromString(pars[i++]);
+		rot         = trimSpacesFromString(pars[i++]);
+		
+		sensitivity = trimSpacesFromString(pars[i++]);
+		touchableID = trimSpacesFromString(pars[i++]);
+		
+		copyOf      = trimSpacesFromString(pars[i++]);
+		replicaOf   = trimSpacesFromString(pars[i++]);
+		pCopyNo     = stoi(trimSpacesFromString(pars[i++]));
+		solidsOpr   = trimSpacesFromString(pars[i++]);
+		
+		mirror      = trimSpacesFromString(pars[i++]);
+		
+		// modifiers - accessed through options/jcard
+		shift = GVOLUMENOTAPPLICABLEENTRY;
+		tilt  = GVOLUMENOTAPPLICABLEENTRY;
+		exist = true;
+		
+		// set file with path if it's a CAD/GDML import
+		importFilename = importPath;
+	}
+}
+
+
+ostream &operator<<(ostream &stream, GVolume gVol)
+{
+	string style = "unknown";
+	if(gVol.style == 0) {
+		style = "wireframe";
+	} else if(gVol.style == 1){
+		style = "solid";
+	}
+	string visibility = "yes";
+	if(!gVol.visible) {
+		visibility = "no";
+	}
+	
+	stream  << endl;
+	stream << "   - Name:            "    << gVol.name     << "  -  " <<  gVol.description << endl;
+	stream << "   - Mother:          "    << gVol.mother      << endl;
+	stream << "   - Type:            "    << gVol.type        << endl;
+	stream << "   - Parameters:      "    << gVol.parameters  << endl;
+	stream << "   - Col, Vis, Style: "    << gVol.color   << ", " << visibility << ", "  << style << endl;
+	stream << "   - Material:        "    << gVol.material << endl;
+	stream << "   - E.M. Field:      "    << gVol.emfield << endl;
+	stream << "   - Position (cm):   "    << gVol.pos << endl;
+	stream << "   - Rotation:        "    << gVol.rot << endl;
+	stream << "   - Sensitivity:     "    << gVol.sensitivity << endl;
+	stream << "   - Touchable ID:    "    << gVol.touchableID << endl;
+	
+	return stream;
+}
+
+
+
+
---
+++ gsetup/gvolume.h
@@ -0,0 +1,91 @@
+#ifndef  GVOLUME_H
+#define  GVOLUME_H 1
+
+// c++
+#include <string>
+#include <vector>
+using std::string;
+using std::ostream;
+using std::vector;
+
+#define gVolumeNumberOfParameters 19
+#define GVOLUMENOTAPPLICABLEENTRY "na"
+
+class GVolume
+{
+public:
+	GVolume(vector<string> pars, string importPath = GVOLUMENOTAPPLICABLEENTRY);
+
+private:
+	string           name; ///< Name of the volume.
+	string         mother; ///< Mother Volume name.
+	string    description; ///< Volume Description, for documentation.
+	string importFilename; ///< For imports, filename with path, set with the import factory
+	
+	// solid parameters
+	string        type;    ///< solid type. This follows the GEANT4 definitions
+	string  parameters;    ///< vector of parameters used in the geant4 solid constructor
+
+	// solid visualization style
+	bool        visible;   ///< visibility of the detector: 0=invisible 1=visible
+	int           style;   ///< Visual style: 0=wireframe 1=solid
+	string        color;   ///< 6(7) digits colors in RRGGBB format. Last optional digit is transparency
+
+	// logical attributes
+	string     material;   ///< Volume Material name.
+	string     emfield;    ///< Magnetic Field. Possible choices: "inherit", "fieldName", "noField"
+
+	// physical attributes
+	string          pos;   ///< Position relative to the mother volume
+	string          rot;   ///< Rotation Matrix, defined by rotations along x,y,z axis relative to the mother volume
+	string        shift;   ///< Position modifier
+	string         tilt;   ///< Rotation modifier
+	bool          exist;   ///< Existance modifier
+
+	string  sensitivity;   ///< Assigns digitization type and collection ID
+	string  touchableID;   ///< Assigns identifier to single volume
+
+	// special cases
+	string       copyOf;   ///< name of gvolume to copy from
+	string    replicaOf;   ///< name of gvolume to replica from
+	string    solidsOpr;   ///< solids operation
+	
+	int         pCopyNo;   ///< should be set to 0 for the first volume of a given type
+
+	// mirrors
+	string       mirror;
+
+	friend ostream &operator<<(ostream &stream, GVolume); ///< Logs infos on screen.
+
+public:
+	string getName()       {return name;}
+	string getMother()     {return mother;}
+	string getType()       {return type;}
+	string getParameters() {return parameters;}
+	string getMaterial()   {return material;}
+	int    getPCopyNo()    {return pCopyNo;}
+
+	string getPos()   {return pos;}
+	string getRot()   {return rot;}
+	string getShift() {return shift;}
+	string getTilt()  {return tilt;}
+
+	string  getSensitivity(){return sensitivity;}
+	string  getTouchableID(){return touchableID;}
+
+	// special cases
+	string getCopyOf()     {return copyOf;}
+	string getReplicaOf()  {return replicaOf;}
+	string getSolidsOpr()  {return solidsOpr;}
+
+	// assign modifier
+	void modifyPos(string s)     {shift = s;}
+	void modifyRot(string t)     {tilt  = t;}
+	void modifyExistence(bool e) {exist = e;}
+
+	// imported volumes
+	string getImportedFile() {return importFilename;}
+};
+
+
+#endif
