ad22bef611dabdc725f12c07df37829dd59569a1
maureeungaro <maureeungaro@gmail.com>
2021-10-21 12:17:27 -0400

    fixed JLABSRO warnings / errors

--- ci.sh
+++ ci.sh
@@ -1,21 +1,83 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 # GLIBRARY Continuous Integration
 # ----------------------------
 #
-# Original Instructions:
-# https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action
-# The steps on "main" are pasted on the Github page under "Actions" > set up a workflow yourself
-#
 # To debug this on the container:
 #
-# cp ci.sh ~/mywork
-# docker run -it --rm -v ~/mywork:/jlab/work/mywork jeffersonlab/gemc:3.0 bash
-# cd work/mywork
+# docker run -it --rm jeffersonlab/gemc:3.0 bash
+# git clone http://github.com/gemc/glibrary /root/glibrary && cd /root/glibrary
 # ./ci.sh
 
-# load environment
-source /etc/profile.d/jlab.sh
+
+# load environment if we're on the container
+FILE=/etc/profile.d/jlab.sh
+if test -f "$FILE"; then
+    source "$FILE"
+fi
+
+# using the checked out GLIBRARY
+export GLIBRARY=`pwd`
+
+function compileCadmesh {
+	echo
+	echo Compiling CadMesh
+	./compileCmesh
+}
+
+function compileGLibrary {
+	# getting number of available CPUS
+	copt=" -j"`getconf _NPROCESSORS_ONLN`" OPT=1"
+	echo
+	echo Compiling glibrary with options: "$copt"
+	scons $copt
+	echo Compilation completed, content of lib:
+	ls -l lib/
+}
+
+function checkLibsExistence {
+
+	libExtension=".a"
+	[[ $OSTYPE == 'darwin'* ]] && libExtension=".dylib"
+
+	# cadmesh
+	for lib in assimp cadmesh
+		do
+		ls lib/lib$lib$libExtension
+		if [ $? -ne 0 ]; then
+			echo $lib not present
+		exit 1
+		fi
+	done
+
+	# glibrary
+	libExtension=".a"
+	for lib in eventDispenser g4display g4system gQtButtonsWidget gdata gdynamic ghit goptions \
+	           gparticle gsplash gstreamer gsystem gtouchable gtranslationTable guts textProgressBar
+		do
+		ls lib/lib$lib$libExtension
+		if [ $? -ne 0 ]; then
+			echo $lib not present
+		exit 1
+		fi
+	done
+
+	# plugins
+	libExtension=".gplugin"
+	for lib in gstreamerJLABSROFactory gstreamerROOTFactory gstreamerTEXTFactory
+		do
+		ls lib/$lib$libExtension
+		if [ $? -ne 0 ]; then
+			echo $lib not present
+		exit 1
+		fi
+	done
+
+
+}
+
+
 
 echo
 echo "GLIBRARY Validation: $1"
@@ -23,35 +85,14 @@ echo
 time=$(date)
 echo "::set-output name=time::$time"
 
-cd /root
-git clone http://github.com/gemc/glibrary
-cd glibrary
-export GLIBRARY=`pwd`
-
-echo
-echo Compiling CadMesh
-./compileCmesh
-
-# getting number of available CPUS
-copt=" -j"`getconf _NPROCESSORS_ONLN`" OPT=1"
-echo
-echo Compiling glibrary with options: "$copt"
-scons $copt
-
-echo Compilation completed, content of lib:
-ls -l lib/
-
-# checking on various libraries
-ls lib/libeventDispenser.a lib/libgdata.a lib/libguts.a lib/libassimp.so lib/libgstreamer.a
-if [ $? -ne 0 ]; then
-  echo compilation not completed
-  exit 1
-fi
-
-
-# checking on the streamers plugins
-ls lib/gstreamerJLABSROFactory.gplugin lib/gstreamerROOTFactory.gplugin lib/gstreamerTEXTFactory.gplugin
-if [ $? -ne 0 ]; then
-  echo gstreamers missing
-  exit 1
+if [ $# -eq 1 ]; then
+	echo "Running check: "$1
+	if [ $1 == "libs" ]; then
+		checkLibsExistence
+	fi
+else
+	echo "Running all checks"
+	compileCadmesh
+	compileGLibrary
+	checkLibsExistence
 fi
