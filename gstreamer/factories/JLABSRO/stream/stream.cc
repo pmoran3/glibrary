2db868c7f83ecf2e9c39ab7aeb0a3fcef578270b
Patrick Moran <phm5024@gmail.com>
2021-10-27 01:56:37 -0400

    fill data frame header in startStream

--- gstreamer/factories/JLABSRO/stream/stream.cc
+++ gstreamer/factories/JLABSRO/stream/stream.cc
@@ -5,8 +5,24 @@ bool GstreamerJSROFactory::startStream(const GFrameDataCollection* frameRunData)
 {
 	if(ofile == nullptr) return false;
 
-	// fill dataFrameHeader here or in publishFrameHeader
+	const GFrameDataCollectionHeader* header = frameRunData->getHeader();
+	long int frameID = header->getFrameID();
 
+	const std::vector<GIntegralPayload*> *payload = frameRunData->getIntegralPayload();
+
+	// fill dataFrameHeader here or in publishFrameHeader
+        DataFrameHeader dfh;
+
+	dfh.source_id = 0;
+        dfh.magic = 0xC0DA2019;
+        dfh.format_version = 257;
+        dfh.flags = 0;
+        dfh.record_counter = llswap(frameID);
+        dfh.ts_sec = llswap((frameID * 65536) / static_cast<int>(1e9));
+        dfh.ts_nsec = llswap((frameID * 65536) % static_cast<int>(1e9));
+	dfh.payload_length = payload->size() * sizeof(unsigned int);
+	dfh.compressed_length = dfh.payload_length;
+	dfh.total_length = dfh.compressed_length + sizeof(DataFrameHeader) - 4; 
 
 	return true;
 }
