a0938e8c04b6c071713689e959e121d4e45022af
Patrick Moran <phm5024@gmail.com>
2022-05-10 16:26:39 -0400

    write super magic only once

--- gstreamer/factories/JLABSRO/stream/frameHeader.cc
+++ gstreamer/factories/JLABSRO/stream/frameHeader.cc
@@ -12,9 +12,7 @@ bool GstreamerJSROFactory::publishFrameHeader(const GFrameDataCollectionHeader *
 	// fill dataFrameHeader here or in startStream
 	//vector<unsigned int> frame_data;
 	//	dataFrameHeader = *reinterpret_cast<DataFrameHeader*>(frame_data.data());
-	std::vector<std::uint32_t> const super_magic = {0xC0DA2019, 0XC0DA0001};
-	
-	ofile->write(reinterpret_cast<const char*>(super_magic.data()), sizeof(std::uint32_t) * 2);
+
 	ofile->write(reinterpret_cast<const char*>(frame_data.data()),  sizeof(DataFrameHeader));
 	
 	return true;
